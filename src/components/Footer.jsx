df2972d1ef2707bca3c7b9a4d51188a3a6a7fe94
KavitaOdedara <kavitaodedara@gmail.com>
2025-03-08 17:18:26 +0530

    Changed year in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
                 <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
                     <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
                     <span className="block text-sm text-gray-500 sm:text-center">
-                        © 2024 <a href="https://flowbite.com/" className="hover:underline">Learniverse™</a> Developed by Kavita Odedara @ 3Way Technologies.
+                        © 2025 <a href="https://flowbite.com/" className="hover:underline">Learniverse™</a> Developed by Kavita Odedara @ 3Way Technologies.
                     </span>
                 </div>
             </footer>
